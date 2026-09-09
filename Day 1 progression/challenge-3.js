518ce104be4c0f8c42a429a262a553ea454e2fe9
karimaa--ab <abhkarima091@gmail.com>
2026-09-09 05:22:59 +0100

    add challenge 3

---
+++ Day 1 progression/challenge-3.js
@@ -0,0 +1,11 @@
+// Challenge 3: Swap Two Values
+
+let firstColor = "red";
+let secondColor = "blue";
+let temp = firstColor;
+
+firstColor = secondColor;
+secondColor = temp;
+
+console.log(firstColor);
+console.log(secondColor);
