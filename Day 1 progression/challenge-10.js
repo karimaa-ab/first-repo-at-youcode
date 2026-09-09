006e3a7a8830015fc707abf1baad81791109bdae
karimaa--ab <abhkarima091@gmail.com>
2026-09-09 06:42:36 +0100

    add challenge 10

---
+++ Day 1 progression/challenge-10.js
@@ -0,0 +1,9 @@
+//Challenge 10: Apply a Discount
+
+let totalPrice = 600;
+
+if (totalPrice >= 500) {
+    totalPrice = totalPrice - (totalPrice * 0.10);
+}
+
+console.log("Final price: " + totalPrice + " MAD");
